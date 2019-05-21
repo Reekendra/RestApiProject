3d914c58d5be0fe80164af9e3a670ecd057f1c2c
Reekendra yadav <reekendray461@gmail.com>
2019-05-21 14:28:00 +0530

    first commit

---
+++ api/routes/orders.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const Order = require("../models/order");
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Orders were fetched"
+  });
+});
+router.post("/", (req, res, next) => {
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    product: req.body.productId
+  });
+  order
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+router.get("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Order details",
+    orderId: req.params.orderId
+  });
+});
+router.delete("/:orderId", (req, res, next) => {
+  res.status(200).json({
+    message: "Order Deleted",
+    orderId: req.params.orderId
+  });
+});
+module.exports = router;
